8b94769669e5d9f9031d469994599aa851190c34
Philipp Nowak <git@lit.plus>
2024-09-20 21:58:14 +0200

    Adjust code smells in template

--- src/main/java/plus/lit/azure/functions/HttpTriggerJava.java
+++ src/main/java/plus/lit/azure/functions/HttpTriggerJava.java
@@ -1,8 +1,15 @@
 package plus.lit.azure.functions;
 
-import java.util.*;
-import com.microsoft.azure.functions.annotation.*;
-import com.microsoft.azure.functions.*;
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+
+import java.util.Optional;
 
 /**
  * Azure Functions with HTTP Trigger.
@@ -15,13 +22,14 @@ public class HttpTriggerJava {
      */
     @FunctionName("HttpTriggerJava")
     public HttpResponseMessage run(
-            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.FUNCTION) HttpRequestMessage<Optional<String>> request,
-            final ExecutionContext context) {
-        context.getLogger().info("Java HTTP trigger processed a request.");
+            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.FUNCTION)
+            HttpRequestMessage<Optional<String>> request,
+            ExecutionContext context
+    ) {
+        context.getLogger().info("Processing request!");
 
-        // Parse query parameter
-        String query = request.getQueryParameters().get("name");
-        String name = request.getBody().orElse(query);
+        var query = request.getQueryParameters().get("name");
+        var name = request.getBody().orElse(query);
 
         if (name == null) {
             return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
